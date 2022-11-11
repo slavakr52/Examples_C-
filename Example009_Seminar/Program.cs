b4ec561562a0638afca40c0ced7864e69a91f32d
slavakr52 <slavakr52@mail.ru>
2022-11-11 21:26:45 +0300

    v3

--- Example009_Seminar/Program.cs
+++ Example009_Seminar/Program.cs
@@ -35,3 +35,43 @@ while (N2 <= N)
 }
 */
 
+//Задача: принять на вводе трехзначное число и вывести его последнюю цифру
+
+/*
+Console.WriteLine("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+int S = N%10;
+Console.Write($"Последняя цифра числа: {S}");
+*/
+
+//Задача: Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+/*
+Console.WriteLine("Введите первое число: ");
+int N1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int N2 = int.Parse(Console.ReadLine()!);
+int N3 = N2*N2;
+
+if (N3 == N1)
+{
+    Console.WriteLine("Да");
+}
+else
+    Console.WriteLine("Нет ");
+*/
+
+//Задача: Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+/*
+Console.WriteLine("Введите номер дня недели: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (N == 1) Console.Write ("Понедельник");
+if (N == 2) Console.Write ("Вторник");
+if (N == 3) Console.Write ("Среда");
+if (N == 4) Console.Write ("Четверг");
+if (N == 5) Console.Write ("Пятница");
+if (N == 6) Console.Write ("Суббота");
+if (N == 7) Console.Write ("Воскресенье");
+*/
