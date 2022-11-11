d18e4c2a6a12a45d4ce6e301c8c079d8cd79ee35
slavakr52 <slavakr52@mail.ru>
2022-11-11 21:05:06 +0300

    v2

--- Example009_Seminar/Program.cs
+++ Example009_Seminar/Program.cs
@@ -8,11 +8,30 @@
 
 //Вариант 1
 
+/* 
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
-
 int sqr = number * number;
-
 Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+*/
 
+//Вариант 2 с использованием библиотек
+
+/*
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
+*/
+
+//Задача: ввести число X, на выводе получить интервал от -X до X
+
+/*
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int N2 = -N;
+while (N2 <= N)
+{
+    Console.Write($"{N2} ");
+    N2++;
+}
+*/
 
