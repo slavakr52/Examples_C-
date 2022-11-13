e82057fb256ff0c508bc5dc633f1bec987bd9ca6
slavakr52 <slavakr52@mail.ru>
2022-11-13 23:28:46 +0300

    Update after lecture 2 010+011

---
+++ Example010_IntroMethod/Program.cs
@@ -0,0 +1,85 @@
+﻿//Найти максимальное число. Решаем хаотичным методом:
+
+/*
+int a1 = 11;
+int b1 = 12;
+int c1 = 13;
+int a2 = 21;
+int b2 = 22;
+int c2 = 23;
+int a3 = 31;
+int b3 = 32;
+int c3 = 33;
+
+int max = a1;
+
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
+*/
+
+// Теперь воспользуемся функциями:
+
+/*
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 11;
+int b1 = 12;
+int c1 = 13;
+int a2 = 21;
+int b2 = 22;
+int c2 = 23;
+int a3 = 31;
+int b3 = 32;
+int c3 = 33;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
+*/
+
+//То же самое, только используем функцию внутри аргумента (отсылка к функциональному программированию)
+
+/*
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 11;
+int b1 = 12;
+int c1 = 13;
+int a2 = 21;
+int b2 = 22;
+int c2 = 233;
+int a3 = 31;
+int b3 = 32;
+int c3 = 33;
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
+*/
+
---
+++ Example011_IntroArray/Program.cs
@@ -0,0 +1,84 @@
+﻿
+/*
+// Индексы:    0   1   2   3   4  .... 
+int[] array = {11, 21, 31, 41, 51, 61, 71, 81, 91};
+array[0] = 12; //Так можно записать новое значение элемента массива
+Console.WriteLine(array[0]);// Выводим новое значение
+*/
+
+//Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find
+
+/*
+int[] array = { 1, 2, 8, 4, 5, 6, 7, 8 };
+
+int n = array.Length;
+int find = 8;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //Прерывает цикл, если искомый элемент уже найден
+    }
+    index++; //index = index + 1
+}
+*/
+
+//Метод заполнения массива рандомными числами и его печати на экран
+
+/*
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+// Следующий метод IndexOf будет искать среди рандомного массива число find и выводить его индекс
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Для того чтобы при отсутствии нужного элемента выводился не 0, а -1, и мы точно знали, что такого элемента в массиве нет
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int [10]; //Создаётся новый массив из 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos  = IndexOf(array, 4);
+Console.WriteLine(pos);
+*/
+
+
