d9f7f03d8ae385092c94fe981e9dafc82e236b4d
slavakr52 <slavakr52@mail.ru>
2022-12-06 21:48:05 +0300

    Seminar 8 update

---
+++ Example019_Seminar7/Program.cs
@@ -0,0 +1,341 @@
+﻿//Задайте двумерный массив размеров m*n, 
+//заполненный случайными целыми числами
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] Array = GetArray(rows, columns, 0, 10);
+PrintArray(Array);
+
+
+// Метод создания двумерного массива
+int [,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] Array = GetArray(rows, columns, 0, 10);
+PrintArray(Array);
+
+
+// Метод создания двумерного массива
+int [,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = i + j;
+        }
+    }
+    return result;
+}
+
+
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса 
+// чётные, и замените эти элементы на их квадраты.
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] Array = GetArray(rows, columns, 0, 10);
+int[,] Array2 = FillArray2(Array);
+PrintArray(Array);
+ChangeArray(Array2);
+Console.WriteLine();
+PrintArray(Array2);
+
+
+
+// Метод создания двумерного массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray2(int[,] arrayX)
+{
+    int[,] NewArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            NewArray[i, j] = arrayX[i, j];
+        }
+    }
+    return NewArray;
+}
+
+void ChangeArray(int[,] arrayZ)
+{
+    for (int i = 0; i < arrayZ.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayZ.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 ==0) arrayZ[i,j] = (int) Math.Pow(arrayZ[i,j], 2);
+        }
+    }
+}
+*/
+
+// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] Array = GetArray(rows, columns, 0, 10);
+int[,] Array2 = FillArray2(Array);
+PrintArray(Array);
+
+
+// Метод создания двумерного массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray2(int[,] arrayX)
+{
+    int[,] NewArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            NewArray[i, j] = arrayX[i, j];
+        }
+    }
+    return NewArray;
+}
+
+int SumOfDiagonalElements(int[,] arrayZ)
+{
+    int sum = 0;
+    for (int i = 0; i < arrayZ.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayZ.GetLength(1); j++)
+        {
+            if(i == j) sum = sum + arrayZ[i,j];
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма диагональных элементов равна {SumOfDiagonalElements(Array)}");
+*/
+
+// Задать двумерный массив и сделать сортировку элементов по возрастанию в нечетных столбцах.
+
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] Array = GetArray(rows, columns, 0, 9);
+int[,] Array2 = FillArray2(Array);
+PrintArray(Array);
+Console.WriteLine();
+SortArray(Array2);
+PrintArray(Array2);
+
+// Метод создания двумерного массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray2(int[,] arrayX)
+{
+    int[,] NewArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            NewArray[i, j] = arrayX[i, j];
+        }
+    }
+    return NewArray;
+}
+
+int[,] SortArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (j % 2 != 0)
+        {
+
+            int temp = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1) - 1; k++)
+                {
+                    if (array[i, j] < array[i, j + 1])   //ПЕРЕДЕЛАТЬ !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    {
+                        temp = array[i, j + 1];
+                        array[i, j + 1] = array[i, j];
+                        array[i, j] = temp;
+                    }
+                }
+            }
+
+        }
+    }
+    return array;
+}
+
+// void GetSortColumnNec(int[,] array)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         if (j % 2 != 0)
+//         {
+//             bool flag = true;
+//             while (flag)
+//             {
+//                 flag = false;
+//                 for (int i = 0; i < array.GetLength(0) - 1; i++)
+//                 {
+//                     if (array[i, j] > array[i + 1, j])
+//                     {
+//                         int stakan = array[i, j];
+//                         array[i, j] = array[i + 1, j];
+//                         array[i + 1, j] = stakan;
+//                         flag = true;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
---
+++ Example020_Seminar8/Program.cs
@@ -0,0 +1,125 @@
+﻿
+Console.WriteLine();
+Console.WriteLine("Введите количество строк массива, затем количество столбцов массива: ");
+int rows2 = int.Parse(Console.ReadLine()!);
+int columns2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[,] ArrayMain = CreateRandomArray(rows2, columns2);
+PrintArray(ArrayMain);
+Console.WriteLine();
+
+// ==========================**Задача 53:** Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+/*
+ChangeArray(ArrayMain);
+PrintArray(ArrayMain);
+*/
+
+// ==========================**Задача 55:** Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести 
+//сообщение для пользователя.
+
+/*
+ReplaceRowsWithColumns(ArrayMain);
+Console.WriteLine();
+PrintArray(ArrayMain);
+*/
+
+// ========================== **Задача** Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+
+Console.WriteLine(ArrayDell(ArrayMain));
+
+
+//==============================Методы==============================
+
+int[,] ReplaceRowsWithColumns(int[,] array)
+{
+    int temp = 0;
+    if (array.GetLength(0) != array.GetLength(1)) Console.WriteLine("Невозможно выполнить!");
+    else
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = i + 1; j < array.GetLength(1); j++)
+            {
+                temp = array[i, j];
+                array[i, j] = array[j, i];
+                array[j, i] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[,] CreateRandomArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(10, 100);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+    }
+    return array;
+}
+
+int[,] ArrayDell(int[,] array){
+    int numbeMin = array[0,0];
+    int num1=0 , num2 =0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++){
+            if (numbeMin>array[i,j])
+            { 
+                numbeMin=array[i,j];
+                num1=i ;
+                num2 = j;
+            }
+        }
+    }
+    int [,] array2 = new int[array.GetLength(0)-1,array.GetLength(1)-1];
+    for (int i = 0 , x = 0 ; i < array.GetLength(0); x++, i++)
+    {
+        if ( i== num1) {
+            x--;
+            continue;
+        }
+        for (int j = 0 , y = 0; j < array.GetLength(1); y++, j++){
+            if ( j== num2) {
+            y--;
+            continue;
+            }
+            array2[x,y]=array[i,j];
+        }
+    }
+    return array2;
+}
